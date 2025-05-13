835d52e91ea695b067cd9e473c2ba14d5769558a
Binaya Bhandari <binaybhandari517@gmail.com>
2025-05-13 09:05:48 +0545

    Function Type

---
+++ 01. variables/app.js
@@ -0,0 +1,52 @@
+// Ways to declare a variablel
+// var, let, const
+
+let fristName = "Binaya";
+let lastName = "Bhandari";
+
+console.log(fristName, lastName);
+
+let age = 23;
+
+console.log(age);
+
+//Reassigning a variable
+age = 31;
+
+console.log(age);
+
+let score;
+
+score = 100;
+
+console.log(score);
+
+if (true) {
+  score = score + 1;
+}
+
+console.log(score);
+
+const x = 10;
+
+const arr = [1, 2, 3, 4];
+
+arr.push(5);
+console.log(arr);
+
+const person = {
+  name: "Binaya",
+};
+
+person.name = "John";
+person.email = "binaya@gmail.com";
+console.log(person);
+
+// Declare multiple variables at once
+let a, b, c;
+
+const d = 10,
+  e = 20,
+  f = 30;
+
+console.log(d);
